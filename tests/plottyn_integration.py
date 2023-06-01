cc37428cc471381dfdcbde2fc0834a1007582767
Alex Racape <alex.racape@gmail.com>
2023-06-01 16:04:28 -0400

    More coverage!

--- tests/plottyn_integration.py
+++ tests/plottyn_integration.py
@@ -4,65 +4,72 @@ Test the functionality of the client using a custom delegate and callback functi
 Designed to interact with PlottyN server to create a 3d scatter chart
 """
 
+import logging
 import unittest
 import queue
 
 from penne import Client, TableID
 from penne.delegates import TableID
-from .test_delegates import TableDelegate
+from tests.test_delegates import TableDelegate
 
 # Create callback functions
 # Not sure about 'response' - cleaner way?
-table = TableID(0, 0)
 points = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5],
           [(0, 1, 1), (0, 1, 1), (0, 1, 1), (0, 1, 1), (0, 1, 1)]]
 
 
-# Callbacks
-def create_table():
-    client.invoke_method("new_point_plot", points, on_done=subscribe)
+def run_basic_operations(table: TableID):
 
+    # Callbacks
+    def create_table():
+        client.invoke_method("new_point_plot", points, on_done=subscribe)
 
-def subscribe(response):
-    client.state[table].subscribe(on_done=plot)
+    def subscribe(response):
+        client.state[table].subscribe(on_done=plot)
 
+    def plot():
+        client.state[table].plot(on_done=insert_points)
 
-def plot():
-    client.state[table].plot(on_done=insert_points)
+    def insert_points():
+        client.state[table].request_insert(
+            row_list=[[8, 8, 8, .3, .2, 1, .05, .05, .05], [9, 9, 9, .1, .2, .5, .02, .02, .02, "Annotation"]],
+            on_done=update_rows
+        )
 
+    def update_rows():
+        client.state[table].request_update([3], [[4, 6, 3, 0, 1, 0, .1, .1, .1, "Updated this row"]],
+                                           on_done=remove_row)
 
-def insert_points():
-    client.state[table].request_insert(
-        row_list=[[8, 8, 8, .3, .2, 1, .05, .05, .05], [9, 9, 9, .1, .2, .5, .02, .02, .02, "Annotation"]],
-        on_done=update_rows
+    # def get_selection():
+    #     client.state[table].request_update_selection("Test Select", [1, 2, 3], on_done=remove_row)
+
+    def remove_row():
+        client.state[table].request_remove([2], on_done=shutdown)
+
+    def shutdown():
+        client.is_active = False
+
+    # Set up logging
+    logging.basicConfig(
+        format="%(message)s",
+        level=logging.DEBUG
     )
 
+    # Main execution loop
+    del_hash = {"tables": TableDelegate}
+    with Client("ws://localhost:50000", del_hash, on_connected=create_table, strict=True) as client:
+        while client.is_active:
+            try:
+                callback_info = client.callback_queue.get(block=False)
+            except queue.Empty:
+                continue
+            print(f"Callback: {callback_info}")
+            callback, args = callback_info
+            callback(args) if args else callback()
 
-def update_rows():
-    client.state[table].request_update([3], [[4, 6, 3, 0, 1, 0, .1, .1, .1, "Updated this row"]],
-                                       on_done=get_selection)
-
-
-def get_selection():
-    client.state[table].request_update_selection("Test Select", [1, 2, 3], on_done=remove_row)
-
-
-def remove_row():
-    client.state[table].request_remove([2], on_done=shutdown)
-
-
-def shutdown():
-    client.shutdown()
+    print(f"Finished Testing")
 
 
-del_hash = {"tables": TableDelegate}
-with Client("ws://localhost:50000", del_hash, strict=True) as client:
-    while client.is_active:
-        try:
-            callback_info = client.callback_queue.get(block=False)
-        except queue.Empty:
-            continue
-        callback, args = callback_info
-        callback(args) if args else callback()
-
-print(f"Finished Testing")
+if __name__ == "__main__":
+    table = TableID(0, 0)
+    run_basic_operations(table)
---
+++ tests/servers.py
@@ -0,0 +1,206 @@
+
+import weakref
+
+import pytest
+import pandas as pd
+
+from rigatoni import *
+
+
+def simple_method(server: Server, context, *args):
+    return "Method on server called!"
+
+
+def move_method(server: Server, context, x, y, z):
+    print(f"Moving {x}, {y}, {z}")
+    return "Moved!"
+
+
+def new_point_plot(server: Server, context: dict, xs, ys, zs, colors=None, sizes=None):
+    # Create component and state and get delegate reference
+    tbl_delegate = server.create_component(
+        Table,
+        name="Custom Table",
+        meta="Table for testing",
+        methods_list=[
+            server.get_component_id(Method, "noo::tbl_subscribe"),
+            server.get_component_id(Method, "noo::tbl_insert")
+        ],
+        signals_list=[
+            server.get_component_id(Signal, "noo::tbl_reset"),
+            server.get_component_id(Signal, "noo::tbl_updated"),
+            server.get_component_id(Signal, "noo::tbl_rows_removed"),
+            server.get_component_id(Signal, "noo::tbl_selection_updated")
+        ]
+    )
+
+    # Set default colors and sizes
+    if not colors:
+        colors = [0.0, 0.0, 0.0] * len(xs)
+    if not sizes:
+        sizes = [[.02, .02, .02] for i in xs]
+    # Get values for dataframe
+    color_cols = list(zip(*colors))
+    size_cols = list(zip(*sizes))
+
+    data = {
+        "x": xs,
+        "y": ys,
+        "z": zs,
+        "r": color_cols[0],
+        "g": color_cols[1],
+        "b": color_cols[2],
+        "sx": size_cols[0],
+        "sy": size_cols[1],
+        "sz": size_cols[2],
+        "": [''] * len(xs)
+    }
+
+    tbl_delegate.dataframe = pd.DataFrame(data)
+    print(tbl_delegate.dataframe)
+
+    return 1
+
+
+def subscribe(server: Server, context: dict):
+    # Try to get delegate from context
+    try:
+        tbl_id = TableID(*context["table"])
+        delegate: CustomTableDelegate = server.delegates[tbl_id]
+    except Exception:
+        raise Exception(MethodException(code=-32600, message="Invalid Request - Invalid Context for Subscribe"))
+
+    tbl: pd.DataFrame = delegate.dataframe
+    types = ["REAL", "REAL", "REAL", "REAL", "REAL", "REAL", "REAL", "REAL", "REAL", "TEXT"]
+
+    # Arrange col info
+    col_info = [TableColumnInfo(name=col, type=type) for col, type in zip(tbl.columns, types)]
+
+    # Formulate response info for subscription
+    init_info = TableInitData(columns=col_info, keys=tbl.index.values.tolist(), data=tbl.values.tolist())
+
+    print(f"Init Info: {init_info}")
+    return init_info
+
+
+def insert(server: Server, context: dict, rows: list[list]):
+    try:
+        tbl_id = TableID(*context["table"])
+        delegate: CustomTableDelegate = server.delegates[tbl_id]
+    except Exception:
+        raise MethodException(-32600, "Invalid Request - Invalid Context for insert")
+
+    # Allow for rows without annotations
+    for row in rows:
+        if len(row) == 9:
+            row.extend([''])
+
+    # Update state in delegate
+    keys = delegate.handle_insert(rows)
+    print(f"Inserted @ {keys}, \n{delegate.dataframe}")
+
+    # Send signal to update client
+    delegate.table_updated(keys, rows)
+
+    return keys
+
+
+class CustomTableDelegate(ServerTableDelegate):
+
+    def __init__(self, server: Server, component: weakref.ReferenceType):
+        super().__init__(server, component)
+        self.dataframe = pd.DataFrame()
+
+    def handle_insert(self, rows: list[list[int]]):
+        next_index = self.dataframe.index[-1] + 1
+        new_index = range(next_index, next_index + len(rows))
+
+        new_df = pd.DataFrame(rows, columns=self.dataframe.columns.values, index=new_index)
+        self.dataframe = pd.concat([self.dataframe, new_df])
+
+        return list(new_index)
+
+    def handle_update(self, keys: list[int], rows: list[list[int]]):
+        for key, row in zip(keys, rows):
+            self.dataframe.loc[key] = row
+
+        return keys
+
+    def handle_delete(self, keys: list[int]):
+        self.dataframe.drop(index=keys, inplace=True)
+
+        return keys
+
+    def handle_clear(self):
+        self.dataframe = pd.DataFrame()
+        self.selections = {}
+
+    def handle_set_selection(self, selection: Selection):
+        self.selections[selection.name] = selection
+
+    # Signals ---------------------------------------------------
+    def table_reset(self, tbl_init: TableInitData):
+        """Invoke table reset signal"""
+
+        data = [tbl_init]
+
+        signal = self.server.get_component_id(Signal, "noo::tbl_reset")
+        self.server.invoke_signal(signal, self.component, data)
+
+    def table_updated(self, keys: list[int], rows: list[list[int]]):
+        data = [keys, rows]
+
+        signal = self.server.get_component_id(Signal, "noo::tbl_updated")
+        self.server.invoke_signal(signal, self.component, data)
+
+    def table_rows_removed(self, keys: list[int]):
+        data = [keys]
+
+        signal = self.server.get_component_id(Signal, "noo::tbl_rows_removed")
+        self.server.invoke_signal(signal, self.component, data)
+
+    def table_selection_updated(self, selection: Selection):
+        data = [selection]
+
+        signal = self.server.get_component_id(Signal, "noo::tbl_selection_updated")
+        self.server.invoke_signal(signal, self.component, data)
+
+
+server_delegates = {
+    Table: CustomTableDelegate
+}
+
+
+test_args = [
+    MethodArg(name="x", doc="How far to move in x", editor_hint="noo::real"),
+    MethodArg(name="y", doc="How far to move in y", editor_hint="noo::real"),
+    MethodArg(name="z", doc="How far to move in z", editor_hint="noo::real")
+]
+
+
+starting_components = [
+    StartingComponent(Method, {"name": "test_method"}, simple_method),
+    StartingComponent(Method, {"name": "test_arg_method", "arg_doc": [*test_args]}, move_method),
+    StartingComponent(Method, {"name": "new_point_plot", "arg_doc": []}, new_point_plot),
+    StartingComponent(Method, {"name": "noo::tbl_subscribe", "arg_doc": []}, subscribe),
+    StartingComponent(Method, {"name": "noo::tbl_insert", "arg_doc": []}, insert),
+    StartingComponent(Method, {"name": "Test Method 4", "arg_doc": []}, print),
+
+    StartingComponent(Signal, {"name": "noo::tbl_reset", "arg_doc": []}),
+    StartingComponent(Signal, {"name": "noo::tbl_updated", "arg_doc": []}),
+    StartingComponent(Signal, {"name": "noo::tbl_rows_removed", "arg_doc": []}),
+    StartingComponent(Signal, {"name": "noo::tbl_selection_updated", "arg_doc": []}),
+    StartingComponent(Signal, {"name": "test_signal"}),
+
+    StartingComponent(Entity, {"name": "test_entity"}),
+    StartingComponent(Entity, {"name": "test_method_entity", "methods_list": [[0, 0]]}),
+    StartingComponent(Material, {"name": "test_material"}),
+    StartingComponent(Table, {"name": "test_table", "methods_list": [[0, 0]], "signals_list": [[0, 0]]}),
+    StartingComponent(Plot, {"name": "test_plot", "simple_plot": "True", "methods_list": [[0, 0]]})
+]
+
+
+@pytest.fixture
+def rig_base_server():
+    with Server(50000, starting_components, server_delegates) as server:
+        yield server
